d151bb9b750c8020d6fbab42106346ef2088e245
Bga <bga.email@gmail.com>
2026-02-21 11:34:25 +0300

    + fix double make_response call

--- dns_generator/ClientHandler.py
+++ dns_generator/ClientHandler.py
@@ -15,5 +15,5 @@ class ClientHandler(threading.Thread):
 
     def run(self):
         resp = self.dns_gen.make_response()
-        self.sock.sendto(self.dns_gen.make_response(), self.client_address)
+        self.sock.sendto(resp, self.client_address)
         print("Request from {0} for {1}".format(self.client_address, self.dns_gen.domain))
