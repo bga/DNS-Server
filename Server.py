d261c6c4b23f371eea22906a3adb2d312e16d9eb
Bga <bga.email@gmail.com>
2026-02-22 12:29:06 +0300

    + argparse and port cli arg

--- Server.py
+++ Server.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 import socket
+import argparse
 
 from dns_generator import ClientHandler
 
 # Global variables
 IP = "127.0.0.1"
-PORT = 53
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=53)
+    args = parser.parse_args()
+    port = args.port
+     
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((IP, PORT))
-    print("DNS Listening on {0}:{1} ...".format(IP, PORT))
+    sock.bind((IP, port))
+    print("DNS Listening on {0}:{1} ...".format(IP, port))
     while True:
         data, address = sock.recvfrom(650)
         data = bytearray(data)
